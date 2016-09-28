f991951d99b6438b53ab92571fc9a5c2b8da0f9f
Rob Bell <rob.bell@asos.com>
2016-09-28 11:21:25 +0100

    Add Game of Life first implementation not using calisthenics

---
+++ Week3/GameOfLife/GameOfLife/GameOfLife.Test/CellShould.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.InteropServices;
+using NUnit.Framework;
+namespace GameOfLife.Test
+{
+    [TestFixture]
+    public class CellShould
+    {
+        [Test]
+        public void DieIfItHasLessThanTwoNeighbours()
+        {
+            var cell = new Cell(State.Alive);
+
+            cell.Tick(1);
+
+            Assert.That(cell.GetState(), Is.EqualTo(State.Dead));
+        }
+
+        [Test]
+        public void LiveIfItHasTwoOrMoreNeighbours()
+        {
+            var cell = new Cell(State.Dead);
+
+            cell.Tick(2);
+
+            Assert.That(cell.GetState(), Is.EqualTo(State.Alive));
+        }
+
+        [Test]
+        public void DieIfItHasMoreThanThreeNeighbours()
+        {
+            var cell = new Cell(State.Alive);
+
+            cell.Tick(4);
+
+            Assert.That(cell.GetState(), Is.EqualTo(State.Dead));   
+        }
+
+        [Test]
+        public void BecomeLiveWhenItIsDeadAndHasThreeNeighbours()
+        {
+            var cell = new Cell(State.Dead);
+
+            cell.Tick(3);
+
+            Assert.That(cell.GetState(), Is.EqualTo(State.Alive));
+        }
+
+        [Test]
+        public void AllowStateToBeSetOnCreation()
+        {
+            var cell = new Cell(State.Alive);
+
+            Assert.That(cell.GetState(), Is.EqualTo(State.Alive));
+        }
+    }
+
+    public enum State   
+    {
+        Dead,
+        Alive
+    }
+
+    public class Cell
+    {
+        private State currentState;
+
+        public Cell(State initialState)
+        {
+            currentState = initialState;
+        }
+
+        public State GetState()
+        {
+            return currentState;
+        }
+
+        public void Tick(int numberOfNeighbours)
+        {
+            if(numberOfNeighbours < 2) currentState = State.Dead;
+
+            if(numberOfNeighbours >= 2) currentState = State.Alive;
+
+            if(numberOfNeighbours >= 4) currentState = State.Dead;
+        }
+    }
+}
---
+++ Week3/GameOfLife/GameOfLife/GameOfLife.Test/UniverseShould.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace GameOfLife.Test
+{
+    [TestFixture]
+    public class UniverseShould
+    {
+        [Test]
+        public void AllowCellsToBeAdded()
+        {
+            var cell = new Cell(State.Dead);
+            var universe = new Universe();
+
+            universe.SetCell(cell, 2, 3);
+
+            Assert.That(universe.GetCell(2, 3), Is.EqualTo(cell));
+        }
+
+        [Test]
+        public void KillCellWhenItHasNoNeighbours()
+        {
+            var universe = new Universe();
+
+            universe.SetCell(new Cell(State.Alive), 1, 3);
+
+            universe.Tick();
+
+            Assert.That(universe.GetCell(1, 3).GetState(), Is.EqualTo(State.Dead));
+        }
+
+        [Test]
+        public void KeepCellAliveWhenItHasTwoNeighbours()
+        {
+            var universe = new Universe();
+
+            universe.SetCell(new Cell(State.Alive), 1, 3);
+            universe.SetCell(new Cell(State.Alive), 2, 3);
+            universe.SetCell(new Cell(State.Alive), 3, 3);
+
+            universe.Tick();
+
+            Assert.That(universe.GetCell(2, 3).GetState(), Is.EqualTo(State.Alive));
+        }
+    }
+
+    public class Universe
+    {
+        private Cell[] cells = new Cell[9];
+
+        public void SetCell(Cell cell, int x, int y)
+        {
+            cells[x] = cell;
+        }
+
+        public Cell GetCell(int x, int y)
+        {
+            return cells[x];
+        }
+
+        public void Tick()
+        {
+            cells[1].Tick(cells.Length);
+        }
+    }
+}
