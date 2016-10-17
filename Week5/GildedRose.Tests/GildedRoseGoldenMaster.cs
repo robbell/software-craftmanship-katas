0cd87414a20401f8f6fc09a43920bbe70de17574
Rob Bell <rob.bell@asos.com>
2016-10-17 11:54:37 +0100

    Add GildedRose refactoring example

---
+++ Week5/GildedRose.Tests/GildedRoseGoldenMaster.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    public class GildedRoseGoldenMaster
+    {
+//        [Test]
+//        public void IsTheSameAsGoldenMaster()
+//        {
+//            var stringBuilder = new StringBuilder();
+//            Console.SetOut(new StringWriter(stringBuilder));
+//
+//            Program.Main(null);
+//
+//            Assert.That(stringBuilder.ToString(), Is.EqualTo(File.ReadAllText(@"C:\_root\GildedRose-Refactoring-Kata\csharp\GildedRose.Tests\GoldenMaster.txt")));
+//        }
+    }
+}
---
+++ Week5/GildedRose.Tests/GildedRoseShould.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    public class GildedRoseShould
+    {
+        [TestCase(0, -1)]
+        [TestCase(2, 1)]
+        [TestCase(1000, 999)]
+        public void ReduceSellInByOneIfNoNameGiven(int sellIn, int expected)
+        {
+            var items = new List<Item> {new Item { SellIn = sellIn } };
+
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.That(items.First().SellIn, Is.EqualTo(expected));
+        }
+
+        [TestCase(10, 8)]
+//        [TestCase(1, -66)] Requires new test case
+        [TestCase(1000, 998)]
+        public void ReduceQualityByTwoIfNoNameGiven(int quality, int expected)
+        {
+            var items = new List<Item> { new Item { Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.That(items.First().Quality, Is.EqualTo(expected));
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(-45, -45)]
+        public void NeverReduceQualityWhenLessOrEqualToZero(int quality, int expected)
+        {
+            var items = new List<Item> { new Item { Quality = quality } };
+
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.That(items.First().Quality, Is.EqualTo(expected));
+        }
+    }
+}
